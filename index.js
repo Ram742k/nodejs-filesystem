6f262a920e69d135ff1bfa881e95ad640a514fe1
Ram742k <ramkumar742k@gmail.com>
2024-06-21 09:39:56 +0530

    index time update

--- index.js
+++ index.js
@@ -11,21 +11,12 @@ if (!fs.existsSync(folderPath)) {
 
 
 
-const getISTTime = () => {
-    const now = new Date();
-    const utcTime = now.getTime() + now.getTimezoneOffset() * 60000;
-    const istOffset = 5.5 * 60 * 60 * 1000; // IST offset in milliseconds
-    const istTime = new Date(utcTime + istOffset);
-  
-    return istTime;
-  };
 // Endpoint to create a text file with current timestamp
 app.get('/createFile', (req, res) => {
-    const istTime = getISTTime();
-    const timestamp = istTime.toISOString().replace(/[:.]/g, '-');
+    const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
     const fileName = `${timestamp}.txt`;
     const filePath = path.join(folderPath, fileName);
-    const fileContent = istTime.toString();
+    const fileContent = new Date().toString();
   
     fs.writeFile(filePath, fileContent, (err) => {
       if (err) {
